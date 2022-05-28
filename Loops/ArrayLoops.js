95e22e435a6c06722dcec260053dee34836d3b42
Code4Blessings <robincw35@gmail.com>
2022-05-28 08:46:56 -0400

    Creayed and updated ArrayLoops file

---
+++ Loops/ArrayLoops.js
@@ -0,0 +1,35 @@
+//For Loops and Arrays
+const examScores = [98, 77, 84, 91, 57, 66];
+
+for(let i=0; i<= examScores.length; i++) {
+    console.log(examScores[i]);
+}
+
+const greekGodStudents = [
+    {
+        firstName: 'Zeus',
+        grade: 86
+    },
+    {
+        firstName: 'Artemis',
+        grade: 97
+    },
+    {
+        firstName: 'Hera',
+        grade: 72
+    },
+    {
+        firstName: 'Apollo',
+        grade: 90
+    },
+];
+
+addedArray = {}
+
+for(let i=0; i < greekGodStudents.length; i++) {
+    let student = greekGodStudents[i];
+    console.log(`${student.firstName} scored a ${student.grade}!`)
+}
+
+
+
--- Loops/ForLoops.js
+++ Loops/ForLoops.js
@@ -1,6 +1,18 @@
 //for loops
 //array = [1,3,5,7,9]
 for(let i=1; i <=10; i++) {
-    console.log(i^2);
+    console.log(i*2);
+}
+
+//Loop Perfect Squares Example
+
+for(let num =0; num <= 20; num++ ){
+    console.log(`${num} x ${num} = ${num * num}`)
+}
+
+//Counting Backwards Loop Example(Decrease each number by 25)
+
+for(let i = 200; i>=0; i-=25) {
+    console.log(i);
 }
 
