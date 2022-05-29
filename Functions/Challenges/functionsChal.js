6858b9c1ec55315cbac8eef54a084b240f489a95
Code4Blessings <robincw35@gmail.com>
2022-05-29 08:21:57 -0400

    Solved getCard function challenge

--- Functions/Challenges/functionsChal.js
+++ Functions/Challenges/functionsChal.js
@@ -35,3 +35,48 @@ function averageValue(arr) {
 
 console.log(averageValue([75, 76, 80, 95, 100]));
 
+// A pangram is a sentence that contains every letter of the alphabet like:
+//"The quick brown fox jumps over the lazy dog"
+//Write a funcyion called isPangram, which checks to see of a given sentence contains every letter of the alphabet.  Make sure you ignore string casting.
+//Test Sample: 'The five boxing wizards jump quickly'
+
+function isPangram(sentence) {
+    //1. loop over the sentence
+    //2. If sentence doesn't contain all letters of the alphabet, return false, otherwise true
+    // const alphabets1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    // const alphabets2 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+    for(let char of 'abcdefghijklmnopqrstuvwxyz') {
+        if(sentence.toLowerCase().indexOf(char) === -1) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPangram('The five boxing wizards jump quick'))
+
+//Write a getCard() function which returns a random playing card object, like:
+// {
+//     value: 'King',
+//     suit: 'Clubs'
+// }
+//Pick a random value from:
+//---A,2,3,4,5,6,7,8,9,10,J,Q,K,
+//Pick a random suit from:
+//---clubs,spades,hearts,diamonds
+//Return both in an object
+
+//Will pick a random value from an array(by the index)
+function getCard() {
+    //Pick a random value from:
+//---A,2,3,4,5,6,7,8,9,10,J,Q,K,
+    values = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+    suits = ['clubs','spades','hearts','diamonds']
+    valCard = values[Math.floor(Math.random() * values.length)]
+    //Pick a random suit from:
+//---clubs,spades,hearts,diamonds
+    suitCard = suits[Math.floor(Math.random() * suits.length)]
+    return {value: `${valCard}`, suit: `${suitCard}`}
+}
+
+console.log(getCard());
