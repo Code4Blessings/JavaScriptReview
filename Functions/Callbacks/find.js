b668928ade5a390db74e72d62ece5cb1c8efd745
Code4Blessings <robincw35@gmail.com>
2022-06-01 19:55:17 -0400

    Craeted find.js file

--- Functions/Callbacks/find.js
+++ Functions/Callbacks/find.js
@@ -1,2 +1,43 @@
 //Find - returns the fist element in the array that saisfies the provided testing function.
 
+//Example
+
+let movies = [
+    "The Fantastic Mr. Fox",
+    "Mr. and Mrs. Smith",
+    "Mrs.Doubtfire",
+    "Mr. Deeds"
+]
+
+let movie = movies.find(movie => movie.includes('Mrs'))
+
+console.log(movie)
+
+//Example #2
+
+const books = [
+    {
+        title: 'Good Omens',
+        authors: ['Terry Pratchett', 'Neil Gaiman'],
+        rating: 4.25
+    },
+    {
+        title: 'Bone: The Complete Edition',
+        authors: ['Jeff Smith'],
+        rating: 4.11
+    },
+    {
+        title: 'American Gods',
+        authors: ['Neil Gaiman'],
+        rating: 4.11
+    },
+    {
+        title: 'A Gentleman in Moscow',
+        authors: ['Amor Towles'],
+        rating: 4.36
+    }
+];
+
+greatBook = books.find(book => book.rating >= 4.3)
+
+console.log(greatBook)
